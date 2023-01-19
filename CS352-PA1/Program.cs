948b39bae9200502af3053d427411ecbeae8e088
Jackson Horton <jacrhort@ut.utm.edu>
2023-01-19 13:07:26 -0600

    Add project files.

---
+++ CS352-PA1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS352_PA1
+{
+    class World
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
